7539b5f8c4bd068e6fa3c0d54051b4111a642799
Андрей Плотников <andrey.p.tomsk@gmail.com>
2023-02-03 16:49:02 +0700

    Рабочая версия без __main__

--- main.py
+++ main.py
@@ -98,14 +98,13 @@ def choice_char_class() -> str:
     return char_class
 
 
-if __main__ == '__main__':
-    run_screensaver()
-    print('Приветствую тебя, искатель приключений!')
-    print('Прежде чем начать игру...')
-    char_name: str = input('...назови себя: ')
-    print(f'Здравствуй, {char_name}! '
-          'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
-    print('Ты можешь выбрать один из трёх путей силы:')
-    print('Воитель, Маг, Лекарь')
-    char_class: str = choice_char_class()
-    print(start_training(char_name, char_class))
+run_screensaver()
+print('Приветствую тебя, искатель приключений!')
+print('Прежде чем начать игру...')
+char_name: str = input('...назови себя: ')
+print(f'Здравствуй, {char_name}! '
+       'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
+print('Ты можешь выбрать один из трёх путей силы:')
+print('Воитель, Маг, Лекарь')
+char_class: str = choice_char_class()
+print(start_training(char_name, char_class))
